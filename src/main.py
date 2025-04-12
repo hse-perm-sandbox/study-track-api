ea458b5842322ebbb75174e40f07ee9b52bf99da
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2025-04-12 22:16:11 +0500

    add fastapi application

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+from src.routes.root import router as test_root_router
+
+
 def start():
-    print("Successful start!")
+    app = FastAPI(
+        title="ADD APP TITLE...",
+        description="API для ...",
+    )
+    app.include_router(router=test_root_router)
+
+    uvicorn.run(app, host="127.0.0.1", port=8080)
 
 
 if __name__ == "__main__":
