720385d392f70f656a11163fe76a35587ac4924c
Hilhool <timur45433@gmail.com>
2025-04-24 08:41:34 +0500

    fixed delete users

--- src/routes/users.py
+++ src/routes/users.py
@@ -5,41 +5,40 @@ router = APIRouter(
     prefix="/api/users",
 )
 
-users = [{"name": "Timur", "age": 18, "id": 1}, {"name": "Kox", "age": 23, "id": 2}]
+users = [
+    {"name": "Timur", "age": 18, "id": 1}, 
+    {"name": "Kox", "age": 23, "id": 2}
+    ]
 
 @router.get(
     "/",
-    summary="Тестовый endpoint",
-    description="Тестовый endpoint для демонстрации работы API",
-    response_description="Возвращает объект с двумя полями: value и description",
+    summary="Получить список пользователей",
+    description="Возвращает всех пользователей в системе",
 )
 async def get_users():
     return users
 
 @router.get(
     "/{id}",
-    summary="Тестовый endpoint",
-    description="Тестовый endpoint для демонстрации работы API",
-    response_description="Возвращает объект с двумя полями: value и description",
+    summary="Получить пользователя по ID",
+    description="Ищет пользователя по указанному ID",
 )
 async def get_user(id: int =Path()):
     return [x for x in users if x["id"] == id]
 
 @router.delete(
     "/{id}",
-    summary="Тестовый endpoint",
-    description="Тестовый endpoint для демонстрации работы API",
-    response_description="Возвращает объект с двумя полями: value и description",
+    summary="Удалить пользователя",
+    description="Удаляет пользователя по указанному ID",
 )
 async def delete_user(id: int =Path()):
-    cur = [x for x in users if x["id"] != id]
-    users = cur
+    global users 
+    users = [x for x in users if x["id"] != id]
 
 @router.post(
     "/",
-    summary="Тестовый endpoint",
-    description="Тестовый endpoint для демонстрации работы API",
-    response_description="Возвращает объект с двумя полями: value и description",
+    summary="Создать нового пользователя",
+    description="Добавляет нового пользователя в список",
 )
 async def post_user(user_data =Body()):
     users.append(user_data)
@@ -47,9 +46,8 @@ async def post_user(user_data =Body()):
 
 @router.patch(
     "/{id}",
-    summary="Тестовый endpoint",
-    description="Тестовый endpoint для демонстрации работы API",
-    response_description="Возвращает объект с двумя полями: value и description",
+    summary="Обновить данные пользователя",
+    description="Изменяет имя или возраст пользователя по ID",
 )
 async def patch_user(id = Path(), user_data =Body()):
     users.append(user_data)
