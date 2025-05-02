5c744b2bb7934a90c2a5f1eca703d4f887f934f0
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2025-05-02 18:08:13 +0500

    add cors

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 
 from src.routes.users import router as user_router
@@ -11,6 +12,13 @@ def start():
         description="Study Track — web-приложение, ориентированное на студентов, с функциями, специально адаптированными под учебный процесс: задачи, категории, приоритеты, напоминания, календарное отображение.",
     )
     app.include_router(router=user_router)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     uvicorn.run(app, host="127.0.0.1", port=8080)
 
