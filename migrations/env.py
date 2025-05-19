5575d977c09e3419e4542bebdb652601ea9188c2
Alexander Mikhailov <45492097+MikhailovAlexander@users.noreply.github.com>
2025-05-19 19:40:46 +0000

    add migrations

---
+++ migrations/env.py
@@ -0,0 +1,61 @@
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
+
+from alembic import context
+
+from src.db.db import Base
+from src.db.models.base import BaseModel
+from src.db.models.user import User
+from src.config import settings
+
+config = context.config
+fileConfig(config.config_file_name)
+target_metadata = Base.metadata
+
+def get_url():
+    return settings.DATABASE_URL
+
+def run_migrations_offline():
+    url = get_url()
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def do_run_migrations(connection):
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_migrations_online():
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section),
+        url=get_url(),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
--- src/db/models/base.py
+++ src/db/models/base.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy import Integer, func
 from sqlalchemy.orm import Mapped, mapped_column
 
-from db.db import Base
+from src.db.db import Base
 
 
 class BaseModel(Base):
