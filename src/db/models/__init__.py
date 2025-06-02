664d5c8bd54939d963050b0dae4fc9e1b95b1630
Чирков Архип РИС-24-3 <aschirkov@edu.hse.ru>
2025-06-02 16:28:00 +0500

    миграция №2

---
+++ src/db/models/__init__.py
@@ -0,0 +1,5 @@
+from src.db.models.base import BaseModel
+from src.db.models.user import User
+from src.db.models.category import Category
+from src.db.models.task import Task
+from src.db.models.notification import Notification
